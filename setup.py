6e81f5510d4ef8aee5fc644b0d3347fece6dc5c6
blackhc <blackhc@gmail.com>
2019-07-10 17:30:49 +0100

    Try to fix Travis.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["tensorflow-gpu~=1.14", "torch~=1.1"],
+    install_requires=["tensorflow~=1.14", "torch~=1.1"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
